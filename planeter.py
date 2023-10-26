d87f8383b04ac7dc293cd037545e45aee12b64cd
hkvie <54811308+hkvie@users.noreply.github.com>
2023-10-26 13:40:23 +0200

    Update planeter.py
    La inn oppgaveteksten i filen

--- planeter.py
+++ planeter.py
@@ -9,5 +9,13 @@ class Planet:
 
 jorden = Planet( "Jorden", 152, 6371)
 
+# Oppgave 1: 
+# Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
+
+# Hva skjer om du skriver print(Jorda)?
+# Hva skjer om du skriver print(Jorda.navn)?
+# Prøv å skriv ut de andre attributtene til klassen
+
+# OBS: pass på rekkefølgen i argumentene til konstruktøren.
 
 
