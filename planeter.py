b1b0c4451d5071a03f40e40c6b94f8c4d9206e79
idahedvardsen <idahedvardsen@gmail.com>
2023-10-27 09:11:05 +0200

    endringer.27.10
    mine endringer

--- planeter.py
+++ planeter.py
@@ -1,21 +1,31 @@
 
 class Planet:
     " En klasse for å beskrive en planet"
-    def __init__(self,navn:str,solavstand:float,radius:float) -> None:
+    def __init__(self,navn:str,solavstand:float,radius:float,antallringer=0) -> None:
 
         self.navn=navn
         self.solavstand=solavstand
         self.radius = radius
+        self.antallringer = antallringer
+        
+    
 
-jorden = Planet( "Jorden", 152, 6371)
+jorda = Planet( "Jorda", 152, 6371)
+mars = Planet("Mars",227.9,3389.5)
+jupiter = Planet("Jupiter",778, 69911)
+print(jorda)
+print(jorda.navn)
+print(jupiter.solavstand)
 
 # Oppgave 1: 
 # Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
+
 
 # Hva skjer om du skriver print(Jorda)?
 # Hva skjer om du skriver print(Jorda.navn)?
 # Prøv å skriv ut de andre attributtene til klassen
 
 # OBS: pass på rekkefølgen i argumentene til konstruktøren.
+
 
 
